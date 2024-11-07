ae77bad98056bb7a1085b7ba35cc21cfe4723e3b
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2024-11-07 17:03:50 -0600

    Update __main__.py
    fix: made functions from `__main__` accessable

--- module/__main__.py
+++ module/__main__.py
@@ -1,25 +1,23 @@
 import argparse
 
-from . import submodule1
-from . import submodule2
-from . import submodule3
+from .submodule1 import __main__ as _submodule1_main
+from .submodule2 import __main__ as _submodule2_main
+from .submodule3 import __main__ as _submodule3_main
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(required=True)
-
 
 subparser1 = subparsers.add_parser("main1")
 subparser2 = subparsers.add_parser("main2")
 subparser3 = subparsers.add_parser("main3")
 
+_submodule1_main.make_parser(subparser1)
+_submodule2_main.make_parser(subparser2)
+_submodule3_main.make_parser(subparser3)
 
-submodule1.make_parser(subparser1)
-submodule2.make_parser(subparser2)
-submodule3.make_parser(subparser3)
-
-subparser1.set_defaults(main=submodule1.__main__.main)
-subparser2.set_defaults(main=submodule2.__main__.main)
-subparser3.set_defaults(main=submodule3.__main__.main)
+subparser1.set_defaults(main=_submodule1_main.main)
+subparser2.set_defaults(main=_submodule2_main.main)
+subparser3.set_defaults(main=_submodule3_main.main)
 
 
 if __name__ == '__main__':
