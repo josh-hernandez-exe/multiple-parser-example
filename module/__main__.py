013131eda955c474eb2fbaf533d5b300edb53c1f
Joshua Hernandez <I.Am.Mr.Josh.Hernandez@gmail.com>
2024-11-08 10:22:31 -0600

    feature: top level parser knows which subparser was used.

--- module/__main__.py
+++ module/__main__.py
@@ -5,16 +5,19 @@ from .submodule2 import __main__ as _submodule2_main
 from .submodule3 import __main__ as _submodule3_main
 
 parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(required=True)
+subparsers = parser.add_subparsers(dest='command', required=True)
 
 subparser1 = subparsers.add_parser("main1")
 subparser2 = subparsers.add_parser("main2")
 subparser3 = subparsers.add_parser("main3")
 
+# In-place create the fields for each subparser, 
+#   using the parser generators for each module
 _submodule1_main.make_parser(subparser1)
 _submodule2_main.make_parser(subparser2)
 _submodule3_main.make_parser(subparser3)
 
+# Define a variable for the main function to run based off which subparser is used.
 subparser1.set_defaults(main=_submodule1_main.main)
 subparser2.set_defaults(main=_submodule2_main.main)
 subparser3.set_defaults(main=_submodule3_main.main)
@@ -23,4 +26,5 @@ subparser3.set_defaults(main=_submodule3_main.main)
 if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
+    print('Running {}'.format(args.command))
     args.main(args)
